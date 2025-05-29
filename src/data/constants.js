f5589432e36ab6dc2985dd20d27f81f9adc79cd6
RajasekharY-9 <yalamillirajasekhar123@gmail.com>
2025-05-29 21:13:26 +0530

    deploy

---
+++ src/data/constants.js
@@ -0,0 +1,211 @@
+export const Bio = {
+  name: "Yalamilli Rajasekhar",
+  roles: [
+    "Full Stack Developer",
+    "Java Developer",
+    "Programmer",
+  ],
+  description:
+    "Java Developer with 3 years of experience in building scalable Java-based enterprise applications using Java 8/17, Spring Boot, Microservices, and RESTful APIs. ",
+  github: "https://github.com/RajasekharY-9",
+  resume:
+    "https://drive.google.com/file/d/1sDqEpws6sth1BkNmWcJjLgObl5iTa-b8/view?usp=drive_link",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+     
+     
+     
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      
+     
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+      {
+        name: "Spring",
+        image: "https://www.vectorlogo.zone/logos/springio/springio-icon.svg",
+      },
+      {
+        name: "Microservices",
+        image: "https://www.vectorlogo.zone/logos/springio/springio-icon.svg",
+      },
+      {
+        name: "IBM DB2",
+        image: "https://www.vectorlogo.zone/logos/springio/springio-icon.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "Postgresql",
+        image: "https://www.postgresql.org/media/img/about/press/elephant.png",
+      },
+     
+    ],
+  },
+
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Dynatrace"  },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Docker",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+      },
+      {
+        name: "TWS",
+        image:
+          "",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://imgs.search.brave.com/1w2IJchsXRTVRHNr_PudF94KNt_ou5JQD16__QEFb6o/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly90b29s/c3FhLmNvbS9nYWxs/ZXJ5L1Bvc3RtYW4v/cG9zdG1hbi5wbmc",
+      },
+     
+      {
+        name: "Figma",
+        image:
+          "https://camo.githubusercontent.com/ed93c2b000a76ceaad1503e7eb9356591b885227e82a36a005b9d3498b303ba5/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6669676d612f6669676d612d69636f6e2e737667",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    //img: "https://www.vectorlogo.zone/logos/amazon/amazon-icon.svg",
+    role: "Systems Engineer",
+    company: "Infosys",
+    date: "June 2022 - Present",
+    desc: "Building scalable microservices at Infosys using Java and Spring Boot, automating deployments withGithub Actions and Docker.",
+    skills: [
+      "Java",
+      "SpringBoot",
+      "Microservices",
+      "TWS",
+      "Mysql",
+      "IBM DB2",
+      "React Js",
+      "Dynatrace",
+      "Linux",
+    ]
+  },
+  
+
+];
+
+export const education = [
+  {
+    id: 0,
+    //img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/Kiit.jpeg?alt=media&token=3839d520-c59d-4341-ad8f-0980c98de4dd",
+    school: "P.B.Siddhartha College of Arts and Science",
+    date: "Jun 2018 - August 2021",
+    grade: "8.94 CGPA",
+    desc: "I completed my B.Sc from P.B.Siddhartha College of Arts and Science where I gained in-depth knowledge of Mathematics and Computer Applications. ",
+    degree: "Bachelor of Science, Mathematics",
+  },
+  
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "RBP",
+    
+    description:
+      "Rules Based Pricing used by Kroger to implement Retail changes. ",
+  
+    tags: [
+      "Java",
+      "Spring",
+      "Angular",
+      "Github",
+      "IBM DB2",
+      "CI/CD",
+      
+    ],
+    category: "web app",
+
+  },
+  {
+    id: 0,
+    title: "CRM",
+   
+    description:
+      "Developed a full-stack web application that allows users to search for Vendors ,Reports ,Centers etc.",
+    
+    tags: ["React Js", "Postgres", "Springboot", "Java", "Redux"],
+    category: "web app",
+
+  },
+  {
+    id: 1,
+    title: "Puma2.0",
+
+    description:
+      "PUMA 2.0 Application used to make contracts with vendors of kroger",
+   
+    tags: [
+      "Java",
+      "Spring",
+      "Angular",
+      "Github",
+      "IBM DB2",
+      "CI/CD",
+    ],
+    category: "web app",
+   
+  },
+
+];
+
---
+++ src/utils/Themes.js
@@ -0,0 +1,24 @@
+export const darkTheme = {
+    bg:"#1C1C27",
+    bgLight: "#1C1E27",
+    primary:"#854CE6",
+    text_primary:"#F2F3F4",
+    text_secondary:"#b1b2b3",
+    card:"#171721",
+    card_light: '#191924',
+    button:"#854CE6",
+    white:"#FFFFFF",
+    black:"#000000",
+}
+export const lightTheme = {
+    bg: "#F9FAFB",              // Light, clean background
+    bgLight: "#FFFFFF",         // Lighter background for sections/cards
+    primary: "#4F46E5",         // Soft Indigo – stylish but readable
+    text_primary: "#1F2937",    // Rich dark gray for primary text
+    text_secondary: "#6B7280",  // Muted gray for secondary text
+    card: "#FFFFFF",            // Clean white cards
+    card_light: "#F3F4F6",      // Slightly off-white for light card variant
+    button: "#4F46E5",          // Primary button color (same as primary)
+    white: "#FFFFFF",
+    black: "#000000",
+}
